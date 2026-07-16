2d5a6f7b2567c4fc243eddb7dcaa46132d356dc6
192425257 <192425257.simats@saveetha.com>
2026-07-16 19:38:17 +0530

    Add complexity function and user input for operations

--- CO1_AT1/Question_1/Source_Code/Q1.py
+++ CO1_AT1/Question_1/Source_Code/Q1.py
@@ -1,1 +1,9 @@
+import math
 
+def complexity(n):
+    return n * math.log2(n)
+
+n = int(input("Enter number of users: "))
+
+print("Approximate operations =", complexity(n))
+print("Time Complexity = Θ(n log n)")
